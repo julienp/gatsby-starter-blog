536b9274a1c654fe02bda1ff79dadd7690a6ce09
Julien Poissonnier <julien@caffeine.lu>
2020-06-03 14:02:45 +0200

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -35,7 +35,7 @@ const SEO = ({ description, lang, meta, title }) => {
         lang,
       }}
       title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title} 8`}
+      titleTemplate={`%s | ${site.siteMetadata.title} 9`}
       meta={[
         {
           name: `description`,
