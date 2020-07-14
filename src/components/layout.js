71f3196098b048cb0c3409bbf4dcdc8ddfcfbd56
Julien Poissonnier <julien@caffeine.lu>
2020-07-14 17:04:16 +0200

    changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,7 +61,7 @@ const Layout = ({ location, title, children }) => {
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
-          HELLO
+          HELLO 2
         <a href="https://www.gatsbyjs.org">Gatsby</a>
       </footer>
     </div>
