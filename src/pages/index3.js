5f9fb24f72bc8fc5499688c38bd2860e0133bbdd
Julien Poissonnier <julien@caffeine.lu>
2020-07-16 14:35:58 +0200

    Update index3.js

--- src/pages/index3.js
+++ src/pages/index3.js
@@ -12,7 +12,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All posts 1" />
+      <SEO title="All posts 8" />
       <Bio />
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug 
